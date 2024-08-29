9037f20ec17b812f98808d3f6234970e3707ecb4
Leiru <urodriguez@idevart.com.mx>
2024-08-28 22:14:34 -0600

    add value in atributte instpector with @IBInspectable

--- ReusableUI/Commons/Header/HeaderView.swift
+++ ReusableUI/Commons/Header/HeaderView.swift
@@ -1,17 +1,11 @@
-//
-//  HeaderView.swift
-//  ReusableUI
-//
-//  Created by Uriel Rodriguez Sandoval on 28/08/24.
-//
-
 import UIKit
 
-enum ShowButtons {
-    case onlyBack
-    case noback
-    case oneBtn
-    case twobutton
+enum ShowButtons: Int {
+    case none = 0
+    case onlyBack = 1
+    case noback = 2
+    case oneBtn = 3
+    case twobutton = 4
 }
 
 class HeaderView: UIView {
@@ -24,41 +18,62 @@ class HeaderView: UIView {
     weak var delegateRightItem: HeaderViewRightProtocols?
     weak var delegateBackItem: HeaderViewBackItemProtocols?
     
+    @IBInspectable var showButtons: Int = ShowButtons.none.rawValue {
+        didSet {
+            configureButtons()
+        }
+    }
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         loadNibContent()
-        
         setupInit()
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         loadNibContent()
-    
         setupInit()
     }
     
     func setupInit() {
-       // conffigutacion inicial
-//        leftItem.isHidden = true
-//        rightItem.isHidden = true
+        configureButtons()
+    }
+    
+    func configureButtons() {
+        let selectedOption = ShowButtons(rawValue: showButtons) ?? .none
+        
+        // Ocultar todos los botones al inicio
+        leftItem.isHidden = true
+        rightItem.isHidden = true
+        backItem.isHidden = true
+        
+        // Mostrar botones según la opción seleccionada
+        switch selectedOption {
+        case .none:
+            break
+        case .onlyBack:
+            backItem.isHidden = false
+        case .noback:
+            leftItem.isHidden = false
+            rightItem.isHidden = false
+        case .oneBtn:
+            rightItem.isHidden = false
+        case .twobutton:
+            leftItem.isHidden = false
+            rightItem.isHidden = false
+        }
     }
     
     @IBAction func leftEvent(_ sender: Any) {
-        if let delegate = delegateLeftItem {
-            delegate.didsearch("")
-        }
+        delegateLeftItem?.didsearch("")
     }
     
     @IBAction func rightEvent(_ sender: Any) {
-        if let delegate = delegateRightItem {
-            delegate.menuEvent()
-        }
+        delegateRightItem?.menuEvent()
     }
     
     @IBAction func backEvent(_ sender: Any) {
-        if let delegate = delegateBackItem {
-            delegate.backEvent()
-        }
+        delegateBackItem?.backEvent()
     }
 }
